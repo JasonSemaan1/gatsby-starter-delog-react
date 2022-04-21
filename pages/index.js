2b3b8466a4efe3a8650921b9ffed3b8995c3b194
Jason Semaan <Jason.Semaan1@gmail.com>
2022-04-21 15:51:29 +1000

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import styles from '../styles/Home.module.css'
+
+export function getStaticProps() {
+  return {
+    props: {
+      scrabble_sql: [],
+    }
+  }
+}
+
+export default function Home({scrabble_sql}) {
+ return (
+    <div className={styles.container}>
+      <h1>Hello world!</h1>
+      <pre>{JSON.stringify(scrabble_sql, null, 2)}</pre>
+    </div>
+  );
+}
